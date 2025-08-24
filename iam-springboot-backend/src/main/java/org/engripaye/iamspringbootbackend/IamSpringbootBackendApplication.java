322216dc158146782217054ed0baf001d6ce955c
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-24 08:54:31 +0100

    backend commit by ENGR. IPAYE

---
+++ iam-springboot-backend/src/main/java/org/engripaye/iamspringbootbackend/IamSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.iamspringbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ iam-springboot-backend/src/test/java/org/engripaye/iamspringbootbackend/IamSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engripaye.iamspringbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
